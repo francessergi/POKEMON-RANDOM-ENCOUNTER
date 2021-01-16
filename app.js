02262e7ff3db79ad2c313ff2e75f96aa3e5ebaf6
francessergi <60177653+francessergi@users.noreply.github.com>
2021-01-16 01:03:50 +0100

    Responsive

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var trainerNum = 2;
+var fons = document.getElementById("encuentro");
+fons.style.height = window.screen.height;
+var img_container = document.getElementById("contenidor-pokemon");
+
+/* var shine = document.createElement("img");
+shine.src = "./img/shiny.gif";
+shine.id = "shine-effect"; */
+
+
 
 //Quan carreguem la pagina accedim a la BD i carreguem les opcions.
 function genera_pokemon() {
     var r = Math.floor(Math.random() * (252 - 1) + 1);
-
     var img = document.createElement('img');
-    var img_container = document.getElementById("contenidor-pokemon");
     var ruta = "./gif/Arxiu" + r + ".gif";
     //var ruta = "./img/" + r + ".png";
     if (shiny(10)) {
         ruta = "./gif/shiny/Arxiu" + r + ".gif"
-        flashRed();
+        reverse_background();
+        /* efecto_shiny(); */
     }
+
     img.src = ruta;
     img.alt = ruta;
     if (img_container.hasChildNodes()) {
@@ -28,6 +37,14 @@ function canvia_trainer() {
     trainerNum == 59 ? trainerNum = 1 : trainerNum++;
 }
 
+function efecto_shiny() {
+    img_container.appendChild(shine);
+    window.setTimeout(function() {
+        shine.classList.remove('shine-effect')
+    }, 500)
+
+}
+
 function shiny(ratio) {
     var num = Math.floor(Math.random() * (ratio - 1) + 1);
     console.log(num);
@@ -41,7 +58,11 @@ function flashRed() {
     window.setTimeout(function() {
         document.body.classList.remove('red')
     }, 100)
+}
 
+function reverse_background() {
+    encuentro.style.backgroundImage = "url(./img/backgrounds/reverse.png)";
+    setTimeout(() => { encuentro.style.backgroundImage = "url(./img/backgrounds/background.png)"; }, 800);
 }
 
 function myFunction(event) {
